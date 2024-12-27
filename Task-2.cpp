ea2fd0859663bf033f312f3bacab56050799face
Devrim-k <dlehddlsw@naver.com>
2024-12-27 15:41:18 +0900

    Add files via upload

--- Task-2.cpp
+++ Task-2.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -7,43 +8,51 @@ class Animal
 {
 public:
 	virtual void makeSound() = 0;
+	virtual ~Animal() {}
 };
 
 class Cat : public Animal {
 public:
 	void makeSound() {
-		cout << "Cat meows: Meow! Meow!" << endl;
+		cout << "Cat : Meow~ Meow~" << endl;
 	}
 };
 
 class Dog : public Animal {
 public:
 	void makeSound() {
-		cout << "Dog meows : Woof!Wood!" << endl;
+		cout << "Dog : Woof! Woof!" << endl;
 	}
 };
 
 class Cow : public Animal {
 public:
 	void makeSound() {
-		cout << "Cow meows : Moo! Moo!" << endl;
+		cout << "Cow : Moo~ Moo~" << endl;
 	}
 };
 
-int main() {
-	Animal* myAnimal;
-	Cat myCat;
-	Dog myDog;
-	Cow myCow;
+void print(Animal* Animal)
+{
+	Animal->makeSound();
+}
 
-	myAnimal = &myCat;
-	myAnimal->makeSound();
+int main() 
+{
+	vector<Animal*> Animals;
+	 Animals.push_back(new Cat());
+	 Animals.push_back(new Dog());
+	 Animals.push_back(new Cow());
 
-	myAnimal = &myDog;
-	myAnimal->makeSound();
+	 for (size_t i = 0; i < Animals.size(); ++i) {
+		 cout << "엔터를 누르면 동물 소리가 출력됩니다" << endl;
+		 cin.get(); //호출
+		 print(Animals[i]);
+	 }
 
-	myAnimal = &myCow;
-	myAnimal->makeSound();
+	 for (size_t i = 0; i < Animals.size(); ++i) {
+		 delete Animals[i];
+	 }
 
 	return 0;
 }
